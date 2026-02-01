675da6d8c05e62f6264e5d72bd89e71399077b17
DiegoOnate1DiegoOnate1 <diegoonate202@gmail.com>
2026-02-01 14:56:36 -0800

    created structs relating to the categories of the data such as date, city, and other stuff

---
+++ criminal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct CrimeRecord {
+    string lurnsak;
+    string incidentDate;
+    string category;
+    string city;
+    bool gangRelated;
+    string incidentId;
+    string longitude;
+    strng latitude;
+    string partCategory;
+
+};
